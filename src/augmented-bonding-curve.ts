9144c2dfb66f3bc03dee07c02edea442880435e4
Mateo Daza <mateodaza@gmail.com>
2022-11-10 20:27:22 -0500

    ABC buy/sell orders queries working with basic schema

--- src/augmented-bonding-curve.ts
+++ src/augmented-bonding-curve.ts
@@ -1,34 +1,39 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   AugmentedBondingCurve,
   MakeBuyOrder,
-  MakeSellOrder
-} from "../generated/AugmentedBondingCurve/AugmentedBondingCurve"
-import { ExampleEntity } from "../generated/schema"
+  MakeSellOrder,
+} from "../generated/AugmentedBondingCurve/AugmentedBondingCurve";
+import { BuyOrder, SellOrder } from "../generated/schema";
 
 export function handleMakeBuyOrder(event: MakeBuyOrder): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = BuyOrder.load(event.transaction.from.toHex());
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new BuyOrder(event.transaction.from.toHex());
 
     // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity.count = BigInt.fromI32(0);
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // entity.count = entity.count + BigInt.fromI32(1)
 
   // Entity fields can be set based on event parameters
-  entity.buyer = event.params.buyer
-  entity.onBehalfOf = event.params.onBehalfOf
+  entity.buyer = event.params.buyer;
+  entity.fee = event.params.fee;
+  entity.onBehalfOf = event.params.onBehalfOf;
+  entity.collateral = event.params.collateral;
+  entity.purchaseAmount = event.params.purchaseAmount;
+  entity.returnedAmount = event.params.returnedAmount;
+  entity.timestamp = event.block.timestamp;
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  entity.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -56,4 +61,20 @@ export function handleMakeBuyOrder(event: MakeBuyOrder): void {
   // - contract.getStaticPricePPM(...)
 }
 
-export function handleMakeSellOrder(event: MakeSellOrder): void {}
+export function handleMakeSellOrder(event: MakeSellOrder): void {
+  let entity = SellOrder.load(event.transaction.from.toHex());
+
+  if (!entity) {
+    entity = new SellOrder(event.transaction.from.toHex());
+    entity.count = BigInt.fromI32(0);
+  }
+  entity.buyer = event.params.seller;
+  entity.fee = event.params.fee;
+  entity.onBehalfOf = event.params.onBehalfOf;
+  entity.collateral = event.params.collateral;
+  entity.sellAmount = event.params.sellAmount;
+  entity.returnedAmount = event.params.returnedAmount;
+  entity.timestamp = event.block.timestamp;
+
+  entity.save();
+}
